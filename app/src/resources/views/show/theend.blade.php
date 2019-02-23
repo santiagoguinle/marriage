8a07fd0e67ff9e9a6044f7f4f270c6288e6911f5
Santiago Guinle <santiago.guinle@ventas-privadas.com>
2019-02-23 20:23:04 -0300

    last page game

--- app/src/resources/views/show/theend.blade.php
+++ app/src/resources/views/show/theend.blade.php
@@ -26,29 +26,20 @@
 @section('content')
 
 <form class="contact3-form validate-form">
-    <span class="contact3-form-title">
+    <span class="contact3-form-title" style="padding-bottom:10px">
         Resultados Finales
     </span>
     
     <div class="wrap-contact3-form-radio wrap-contact3-show-options">
         @foreach ($scores as $key=>$score)
-        <div class="contact3-form-radio m-r-42">
+        <div class="contact3-form-radio m-r-42" style="padding-bottom:0px;">
             <label class="label-radio3" for="radio1">
                   Puesto {{ $key+1}}: {{ $score->player }}
             </label>
         </div>
         @endforeach
     </div>
-    
 
-    <div id="container"></div>
-
-    <div class="wrap-contact3-show-options">
-    </div>
-
-    <div class="container-contact3-form-btn wrap-contact3-show-options">
-        
-    </div>
 </form>
 
 @endsection
